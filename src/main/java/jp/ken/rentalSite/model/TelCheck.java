f7d0cd65c04fc7e53e2dbe9d1c3dfe23b05fb29c
KEN-SE <KEN-SE@fuku-java-008>
2018-12-11 09:21:16 +0900

    TelCheckの新規追加

---
+++ src/main/java/jp/ken/rentalSite/model/TelCheck.java
@@ -0,0 +1,24 @@
+package jp.ken.rentalSite.model;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = TelCheckValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TelCheck {
+
+	String message() default "";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
