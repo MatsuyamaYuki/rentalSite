ab8b4837e1daefade3af52cfdc817cd6e0afb7eb
KEN-SE <KEN-SE@fuku-java-008>
2018-12-14 15:31:12 +0900

    UserInformationDAOImplにqueryの追加

--- src/main/java/jp/ken/rentalSite/dao/UserInformationDAO.java
+++ src/main/java/jp/ken/rentalSite/dao/UserInformationDAO.java
@@ -2,5 +2,5 @@ package jp.ken.rentalSite.dao;
 
 public interface UserInformationDAO<T> {
 
-	public void insertUserInformation(int id,String userMail,String userPassword,int plan,int zip,String userAddress,int userTel,String userName,String userBirthday,String cardName,int cardMonth,int cardYear,int gender);
+	public void insertUserInformation(int id,String userMail,String userPassword,int plan,int zip,String userAddress,int userTel,String userName,String userBirthday,String cardName,String cardNumber,int cardMonth,int cardYear,int gender);
 }
--- src/main/java/jp/ken/rentalSite/dao/UserInformationDAOImpl.java
+++ src/main/java/jp/ken/rentalSite/dao/UserInformationDAOImpl.java
@@ -16,11 +16,28 @@ public class UserInformationDAOImpl implements UserInformationDAO<UserInformatio
 
 	@Override
 	public void insertUserInformation(int id, String userMail, String userPassword, int plan, int zip,
-			String userAddress, int userTel, String userName, String userBirthday, String cardName, int cardMonth,
+			String userAddress, int userTel, String userName,String cardNumber, String userBirthday, String cardName, int cardMonth,
 			int cardYear, int gender) {
 
 		Session session = this.sessionFactory.openSession();
-		Query query = session.createQuery("INSERT ");
+		Query query = session.createQuery(""
+				+ "INSERT INTO rentsite_database.userinformation(userMail,userPassword,plan,zip,"
+				+ "userAddress,userTel,userName,userBirthday,cardName,cardNumber,cardMonth,cardYear,gender) "
+				+ "VALUE('?','?',?,?,'?',?,'?','?','?','?',?,?,?)");
+
+				query.setParameter(0 ,userMail);
+				query.setParameter(1 ,userPassword);
+				query.setParameter(2 ,plan);
+				query.setParameter(3 ,zip);
+				query.setParameter(4 ,userAddress);
+				query.setParameter(5 ,userTel);
+				query.setParameter(6 ,userName);
+				query.setParameter(7 ,userBirthday);
+				query.setParameter(8 ,cardName);
+				query.setParameter(9 ,cardNumber);
+				query.setParameter(10 ,cardMonth);
+				query.setParameter(11 ,cardYear);
+				query.setParameter(12 ,gender );
 
 	}
 
